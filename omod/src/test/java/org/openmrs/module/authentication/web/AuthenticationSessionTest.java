617ec9c157d04a32b63a805440ec615ee90144a2
Michael Seaton <mseaton@pih.org>
2022-10-18 09:29:35 -0400

    Merge in changes from other branch

--- omod/src/test/java/org/openmrs/module/authentication/web/AuthenticationSessionTest.java
+++ omod/src/test/java/org/openmrs/module/authentication/web/AuthenticationSessionTest.java
@@ -26,7 +26,6 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AuthenticationSessionTest extends BaseWebAuthenticationTest {
 
