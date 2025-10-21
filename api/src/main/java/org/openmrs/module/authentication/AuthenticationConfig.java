e51c430b8dd3b9f78966a1fc02cc6d597c2ad55c
Michael Seaton <mseaton@pih.org>
2025-10-21 15:01:37 -0400

    AUT-12 - Add rest session endpoint to default whitelist

--- api/src/main/java/org/openmrs/module/authentication/AuthenticationConfig.java
+++ api/src/main/java/org/openmrs/module/authentication/AuthenticationConfig.java
@@ -261,6 +261,7 @@ public class AuthenticationConfig implements Serializable {
         whitelist.add("/appui/session/getLoginLocations.action");
         whitelist.add("/csrfguard");
         whitelist.add("/spa/**/*");
+        whitelist.add("/ws/rest/v1/session");
         whitelist.add("*.js");
         whitelist.add("*.css");
         whitelist.add("*.gif");
